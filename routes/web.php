b7c986c32a233837dfc009818fa9e066f83b6925
averiechristy <averiechristy2101@gmail.com>
2022-04-23 22:33:14 +0700

    feat : discount management admin

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\DashboardController as UserDashboard;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
 use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
+use App\Http\Controllers\Admin\DiscountController as AdminDiscount;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -53,7 +54,8 @@ Route::middleware(['auth'])->group(function (){
         // admin checkout
         Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
 
-        
+          // admin discount
+          Route::resource('discount', AdminDiscount::class);
     });
 });
 
